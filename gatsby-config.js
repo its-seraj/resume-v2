e8f576d60cca3496425629c15f36bb1f0a9a3b85
seraj-b2 <125463271+seraj-b2@users.noreply.github.com>
2024-12-16 00:23:49 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
         display: 'minimal-ui',
-        icon: 'src/images/logo.png',
+        icon: 'public/psy.png',
       },
     },
     `gatsby-plugin-offline`,
